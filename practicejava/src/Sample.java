125b9bb53f5910d081d242dd1a621ac84f2bf3d0
Shantanu12347394 <96788377+Shantanu12347394@users.noreply.github.com>
2022-08-23 21:33:38 +0530

    Add files via upload

---
+++ practicejava/src/Sample.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author shant
+ */
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello manish chutiya");
+    }
+    
+}
