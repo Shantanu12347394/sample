e981568f561b1492b8050b3292e896ef39734146
Swarup Vishwas <91014156+SwarupVishwas18@users.noreply.github.com>
2022-08-24 19:50:16 +0530

    Update Sample.java

--- practicejava/src/Sample.java
+++ practicejava/src/Sample.java
@@ -9,7 +9,7 @@
  */
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello manish chutiya");
+        System.out.println("Hello World..!!");
     }
     
 }
